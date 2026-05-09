1381b09354fe4c4f742218f7996ebdc4b5abcd6b
kinhowix <kinhowix@gmail.com>
2026-05-09 14:53:34 -0300

    ajuste-app

--- public/sw.js
+++ public/sw.js
@@ -1,16 +1,32 @@
-const CACHE_NAME = 'myfigure-v1';
+const CACHE_NAME = 'myfigure-v2';
 const ASSETS = [
   '/',
   '/index.html',
   '/manifest.json',
-  '/cover.png'
+  '/favicon.svg'
 ];
 
 // Install event
 self.addEventListener('install', (event) => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Activate event (cleanup old caches)
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
     })
   );
 });
